214c8974065fa54b365f053abb8316b0628e5c66
Joey-Weblaw <126876775+Joey-Weblaw@users.noreply.github.com>
2024-10-15 16:18:16 +0200

    Initial commit of Nuxt 3 version of SORANA

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Bienvenue sur SORANA</h1>
+      <UButton to="/plan-comptable">Accéder au Plan Comptable</UButton>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  // Vous pouvez ajouter de la logique ici si nécessaire
+  </script>
---
+++ pages/plan-comptable.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>Plan Comptable</h1>
+      <UButton @click="importPlan">Importer Plan Comptable</UButton>
+      <!-- Ici, nous ajouterons plus tard la logique pour afficher le plan comptable -->
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  const importPlan = () => {
+    // Logique d'importation à implémenter
+    console.log('Importation du plan comptable')
+  }
+  </script>
